400039e2fbfe8a101e5ea04e64580de6cdde396d
RBuster <daveastrickland@gmail.com>
2020-07-29 10:55:06 -0500

    working with firebase

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,27 +3,28 @@ import {emailService} from './lib/resources/emailRA';
 import { Email } from './lib/models/email';
 import * as Ajv from 'ajv';
 import { ResponseCodes } from './lib/const/codes';
-import { check } from 'express-validator';
 import { emailValidationSchema } from './lib/validation/emailValidationConfig';
 const express = require('express');
+const cors = require('cors');
 
 require('dotenv').config();
 
 const app = express();
-
-app.post('/sendEmail',[
-    check('subject').trim().escape(),
-    check('emailAddress').isEmail(),
-    check('message').trim().escape()
-    ], (req: any, res: any) => {
-    let reqBody = req.body;
+app.use(cors({ origin: true }));
+app.post('/sendEmail', (req: any, res: any) => {
+    let reqBody = req.body.data
+    console.info('parsed', reqBody);
     let ajv = new Ajv({allErrors: true});
     let validate = ajv.compile(emailValidationSchema);
     let valid = validate(reqBody);
     if (!valid) {
         res.send({
-            status: ResponseCodes.FAILURE,
-            errorMessage: validate.errors
+            error: {
+                code: 400,
+                message: validate.errors,
+                status: ResponseCodes.FAILURE,
+                fullError: validate
+              }
         });
     }
     let payload: Email = new Email(reqBody);
--- functions/src/lib/resources/emailRA.ts
+++ functions/src/lib/resources/emailRA.ts
@@ -3,24 +3,32 @@ import {Email} from './../models/email'
 import * as sgMail from '@sendgrid/mail';
 
 class EmailService {
-    sendEmail(payload: Email): Promise<EmailResponse> {
+    sendEmail(payload: Email): Promise<any> {
         sgMail.setApiKey(process.env.API_KEY || '');
+        console.log('Payload', payload);
         const msg = {
             from: process.env.FROM_EMAIL || '',
             to: payload.emailAddress,
             subject: payload.subject || 'No subject specified',
             html: payload.message ? `<p>${payload.message}</p>` : `<p>no message specified</p>`
         };
+        console.log('Email MSG', msg);
         return sgMail.send(msg).then((info: any) => {
             return {
-                status: ResponseCodes.SUCCESS
+                data: {
+                    status: ResponseCodes.SUCCESS
+                }
             }
         })
         .catch((err: any) => {
             return {
-                status: ResponseCodes.FAILURE,
-                errorMessage: err.message
-            }
+                error: {
+                  code: err.code,
+                  message: err.message,
+                  status: ResponseCodes.FAILURE,
+                  fullError: err
+                }
+              }
         });
     }
 }
