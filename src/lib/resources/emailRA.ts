78b7ee350d30ffb442a33da1fc14dec6323c1ab2
Dave Strickland <daveastrickland@gmail.com>
2022-08-30 14:14:01 -0500

    more ts

--- src/lib/resources/emailRA.ts
+++ src/lib/resources/emailRA.ts
@@ -6,21 +6,16 @@ class EmailService {
   sendEmail(payload: Email): Promise<any> {
     // sgMail.setApiKey(process.env.API_KEY || '');
     // create reusable transporter object using the default SMTP transport
-    let transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.USER,
-        pass: process.env.PASS,
-      },
+    const transporter = nodemailer.createTransport({
+      sendmail: true,
+      newline: 'unix',
     });
     console.log("Payload", payload);
     const msg = {
       from: process.env.FROM_EMAIL || "",
       to: payload.emailAddress,
       subject: payload.subject || "No subject specified",
-      html: payload.message
-        ? `<p>${payload.message}</p>`
-        : `<p>no message specified</p>`,
+      text: 'I hope this message gets delivered!'
     };
     console.log("Email MSG", msg);
     return transporter
--- src/lib/validation/emailValidationConfig.ts
+++ src/lib/validation/emailValidationConfig.ts
@@ -1,7 +1,10 @@
 export const emailValidationSchema = {
+    type: 'object',
     properties: {
         emailAddress: {type: 'string', format : 'email'},
         subject: {type: ['string', 'null']},
         message: {type: ['string', 'null']}
-    }
+    },
+    required: ['emailAddress'],
+    additionalProperties: false
 }
